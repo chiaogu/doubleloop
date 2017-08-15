f158421101b5dfe8338b83ebc4e2e986d50cd118
Cheng Chiao <chiao@herxun.co>
2017-08-15 22:51:52 +0800

    Add recorder to main component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,6 +3,29 @@ import { ConfigService, Config } from "../../services/config.service";
 import { BrickService } from "../../services/brick.service";
 import { Observable } from "rxjs/Observable";
 import { BrickPressEvent } from "../../components/brick/brick.component";
+import { Subject } from "rxjs/Subject";
+
+class Recorder {
+    recording: boolean = false;
+    events: BrickPressEvent[] = [];
+
+    finish: (events) => void;
+
+    toggle() {
+      this.recording = !this.recording;
+      if(!this.recording) {
+        if(this.finish !== undefined){
+          this.finish(this.events.splice(0));
+        }
+      }
+    }
+
+    input(event) {
+      if(this.recording) {
+        this.events.push(event);
+      }
+    }
+}
 
 @Component({
   selector: 'app-main',
@@ -14,6 +37,10 @@ export class MainComponent implements OnInit {
   config$: Observable<Config>;
 
   bricks$: Observable<any>;
+
+  recorder: Recorder = new Recorder();
+
+  track: BrickPressEvent[] = [];
 
   constructor(
     private config: ConfigService,
@@ -42,9 +69,18 @@ export class MainComponent implements OnInit {
         }
         return config.bricks;
       });
+
+    this.recorder.finish = events => {
+      console.log(events);
+      this.track = events;
+    };
   }
 
   onBrickPress(event: BrickPressEvent) {
-    console.log(event);
+    this.recorder.input(event);
+  }
+
+  toggleRecord(){
+    this.recorder.toggle();
   }
 }
